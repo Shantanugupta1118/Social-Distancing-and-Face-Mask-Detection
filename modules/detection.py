0da61a403d4fdb9b1570fe1a0c5d600ce2d3091d
Prajwal.P <50076923+Prajwal-P@users.noreply.github.com>
2021-07-06 15:23:54 +0530

    improved performance (#1)
    * adding gitignore
    
    * added some comments
    
    * trained new model
    
    * update requirements.txt
    
    * removed settings.json
    
    * deleting __pycache__
    
    * adding __pycache__
    
    * imperformance a bit
    
    * added new line at the end
    
    * removed unused imports
    
    * updated required packages
    
    * removed use of imutils package

--- modules/detection.py
+++ modules/detection.py
@@ -5,80 +5,84 @@ import cv2
 
 
 def detect_people(frame, net, ln, personIdx=0):
-	# grab the dimensions of the frame and  initialize the list of
-	# results
-	(H, W) = frame.shape[:2]
-	results = []
+    # grab the dimensions of the frame and  initialize the list of
+    # results
+    (H, W) = frame.shape[:2]
+    results = []
 
-	# construct a blob from the input frame and then perform a forward
-	# pass of the YOLO object detector, giving us our bounding boxes
-	# and associated probabilities
-	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-		swapRB=True, crop=False)
-	net.setInput(blob)
-	layerOutputs = net.forward(ln)
+    # construct a blob from the input frame and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes
+    # and associated probabilities
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
 
-	# initialize our lists of detected bounding boxes, centroids, and
-	# confidences, respectively
-	boxes = []
-	centroids = []
-	confidences = []
+    # initialize our lists of detected bounding boxes, centroids, and
+    # confidences, respectively
+    boxes = []
+    centroids = []
+    confidences = []
 
-	# loop over each of the layer outputs
-	for output in layerOutputs:
-		# loop over each of the detections
-		for detection in output:
-			# extract the class ID and confidence (i.e., probability)
-			# of the current object detection
-			scores = detection[5:]
-			classID = np.argmax(scores)
-			confidence = scores[classID]
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability)
+            # of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
 
-			# filter detections by (1) ensuring that the object
-			# detected was a person and (2) that the minimum
-			# confidence is met
-			if classID == personIdx and confidence > MIN_CONF:
-				# scale the bounding box coordinates back relative to
-				# the size of the image, keeping in mind that YOLO
-				# actually returns the center (x, y)-coordinates of
-				# the bounding box followed by the boxes' width and
-				# height
-				box = detection[0:4] * np.array([W, H, W, H])
-				(centerX, centerY, width, height) = box.astype("int")
+            # filter detections by (1) ensuring that the object
+            # detected was a person and (2) that the minimum
+            # confidence is met
+            if classID == personIdx and confidence > MIN_CONF:
+                # scale the bounding box coordinates back relative to
+                # the size of the image, keeping in mind that YOLO
+                # actually returns the center (x, y)-coordinates of
+                # the bounding box followed by the boxes' width and
+                # height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
 
-				# use the center (x, y)-coordinates to derive the top
-				# and and left corner of the bounding box
-				x = int(centerX - (width / 2))
-				y = int(centerY - (height / 2))
+                # use the center (x, y)-coordinates to derive the top
+                # and and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
 
-				# update our list of bounding box coordinates,
-				# centroids, and confidences
-				boxes.append([x, y, int(width), int(height)])
-				centroids.append((centerX, centerY))
-				confidences.append(float(confidence))
+                if y < 0:
+                    y = 0
 
-	# apply non-maxima suppression to suppress weak, overlapping
-	# bounding boxes
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
-	#print('Total people count:', len(idxs))
-	# compute the total people counter
-	if People_Counter:
-		human_count = "Human count: {}".format(len(idxs))
-		cv2.putText(frame, human_count, (frame.shape[0] - 170, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255),2)
+                # update our list of bounding box coordinates,
+                # centroids, and confidences
+                boxes.append([x, y, int(width), int(height)])
+                centroids.append((centerX, centerY))
+                confidences.append(float(confidence))
 
-	# ensure at least one detection exists
-	if len(idxs) > 0:
-		# loop over the indexes we are keeping
-		for i in idxs.flatten():
-			# extract the bounding box coordinates
-			(x, y) = (boxes[i][0], boxes[i][1])
-			(w, h) = (boxes[i][2], boxes[i][3])
+    # apply non-maxima suppression to suppress weak, overlapping
+    # bounding boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
+    #print('Total people count:', len(idxs))
+    # compute the total people counter
+    if People_Counter:
+        human_count = "Human count: {}".format(len(idxs))
+        cv2.putText(frame, human_count,
+                    (frame.shape[0] - 170, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
-			# update our results list to consist of the person
-			# prediction probability, bounding box coordinates,
-			# and the centroid
-			r = (confidences[i], (x, y, x + w, y + h), centroids[i])
-			results.append(r)
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
 
-	# return the list of results
-	return results
+            # update our results list to consist of the person
+            # prediction probability, bounding box coordinates,
+            # and the centroid
+            r = (confidences[i], (x, y, x + w, y + h), centroids[i])
+            results.append(r)
+
+    # return the list of results
+    return results
--- train_mask_detector.py
+++ train_mask_detector.py
@@ -18,7 +18,6 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
@@ -27,40 +26,40 @@ import os
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset")
+                help="path to input dataset")
 ap.add_argument("-p", "--plot", type=str, default="plot.png",
-	help="path to output loss/accuracy plot")
+                help="path to output loss/accuracy plot")
 ap.add_argument("-m", "--model", type=str,
-	default="classifier.model",
-	help="path to output face mask detector model")
+                default="classifier.model",
+                help="path to output face mask detector model")
 args = vars(ap.parse_args())
 
 # initialize the initial learning rate, number of epochs to train for,
 # and batch size
-INIT_LR = 1e-4
+INIT_LR = 1e-4  # 0.0001
 EPOCHS = 20
 BS = 32
 
 # grab the list of images in our dataset directory, then initialize
 # the list of data (i.e., images) and class images
 print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
+imagePaths = os.listdir(args["dataset"])
 data = []
 labels = []
 
 # loop over the image paths
 for imagePath in imagePaths:
-	# extract the class label from the filename
-	label = imagePath.split(os.path.sep)[-2]
+    # extract the class label from the filename
+    label = imagePath.split(os.path.sep)[-2]
 
-	# load the input image (224x224) and preprocess it
-	image = load_img(imagePath, target_size=(224, 224))
-	image = img_to_array(image)
-	image = preprocess_input(image)
+    # load the input image (224x224) and preprocess it
+    image = load_img(imagePath, target_size=(224, 224))
+    image = img_to_array(image)
+    image = preprocess_input(image)  # converts pixel values between -1 to 1
 
-	# update the data and labels lists, respectively
-	data.append(image)
-	labels.append(label)
+    # update the data and labels lists, respectively
+    data.append(image)
+    labels.append(label)
 
 # convert the data and labels to NumPy arrays
 data = np.array(data, dtype="float32")
@@ -69,27 +68,27 @@ labels = np.array(labels)
 # perform one-hot encoding on the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
-labels = to_categorical(labels)
+labels = to_categorical(labels)  # converts 1-D vector to 2-D matrix
 
 # partition the data into training and testing splits using 75% of
 # the data for training and the remaining 25% for testing
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
+                                                  test_size=0.20, stratify=labels, random_state=42)
 
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest")
 
 # load the MobileNetV2 network, ensuring the head FC layer sets are
 # left off
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
+                        input_tensor=Input(shape=(224, 224, 3)))
 
 # construct the head of the model that will be placed on top of the
 # the base model
@@ -107,22 +106,22 @@ model = Model(inputs=baseModel.input, outputs=headModel)
 # loop over all layers in the base model and freeze them so they will
 # *not* be updated during the first training process
 for layer in baseModel.layers:
-	layer.trainable = False
+    layer.trainable = False
 
 # compile our model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 
 # train the head of the network
 print("[INFO] training head...")
 H = model.fit(
-	aug.flow(trainX, trainY, batch_size=BS),
-	steps_per_epoch=len(trainX) // BS,
-	validation_data=(testX, testY),
-	validation_steps=len(testX) // BS,
-	epochs=EPOCHS)
+    aug.flow(trainX, trainY, batch_size=BS),
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=(testX, testY),
+    validation_steps=len(testX) // BS,
+    epochs=EPOCHS)
 
 # make predictions on the testing set
 print("[INFO] evaluating network...")
@@ -134,7 +133,7 @@ predIdxs = np.argmax(predIdxs, axis=1)
 
 # show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
+                            target_names=lb.classes_))
 
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
